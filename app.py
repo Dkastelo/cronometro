2d392c69783e94526488daa5a07ea86510775241
Diego Castelo <diegoreboucascastelo@gmail.com>
2025-09-21 20:00:09 -0300

    commit teste

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def homepage():
+    return render_template('homepage.html')
+    
+@app.route('/cronometro')
+def cronometro():
+    return render_template('cronometro.html')
+
+@app.route('/temporizador')
+def temporizador():
+    return render_template('temporizador.html')
 
 if __name__ == '__main__':
     app.run()
